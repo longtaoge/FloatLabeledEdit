f10edb08524590986ab0f2c99666ff82467f3cdc
longtaoge <61852263@qq.com>
2014-10-14 22:35:51 +0800

    FloatLabeledEditText
    FloatLabeledEditText

---
+++ MainActivity/gen/com/wrapp/example/floatlabelededittext/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.wrapp.example.floatlabelededittext;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
